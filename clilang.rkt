a968e147821160b411118a791b6f10c487d2d9f8
Siddhartha <sid@countvajhula.com>
2021-05-26 00:47:27 -0700

    add usage-help

--- clilang.rkt
+++ clilang.rkt
@@ -11,6 +11,7 @@
 (provide read-syntax
          run
          program
+         usage-help
          (rename-out (cli-module-begin #%module-begin))
          (except-out (all-from-out racket/base)
                      #%module-begin)
@@ -20,6 +21,7 @@
   (define src-datums (sequence->list (in-port read port)))
   (define module-datum `(module cli-mod "clilang.rkt"
                           (define ~program "")
+                          (define ~usage-help "")
 
                           ,@src-datums))
   (datum->syntax #f module-datum))
@@ -29,16 +31,22 @@
    (with-syntax ([~program (datum->syntax #'value '~program)])
      #'(set! ~program value))])
 
+(define-syntax-parser usage-help
+  [(_ value)
+   (with-syntax ([~usage-help (datum->syntax #'value '~usage-help)])
+     #'(set! ~usage-help value))])
+
 (define-simple-macro (cli-module-begin EXPR ...)
   (#%module-begin
    EXPR ...))
 
 (define-syntax-parser run
   [(_ ([arg desc] ...) body ...)
-   (with-syntax ([~program (datum->syntax this-syntax '~program)])
+   (with-syntax ([~program (datum->syntax this-syntax '~program)]
+                 [~usage-help (datum->syntax this-syntax '~usage-help)])
      #'(parse-command-line ~program
                            (current-command-line-arguments)
-                           null
+                           `((usage-help ,~usage-help))
                            (λ (options arg ...)
                              body
                              ...)
--- dummy.rkt
+++ dummy.rkt
@@ -1,6 +1,9 @@
+#!/usr/bin/env racket
 #lang reader "clilang.rkt"
 
 (program "dum dum")
+
+(usage-help "This is a fun script.")
 
 (run ()
      (displayln "Hello there!"))
