fa3eb221c48122aa36f9ea77aadc5aa26d02e42f
Siddhartha <sid@countvajhula.com>
2021-06-01 18:30:33 -0700

    rudimentary support for `once-any`

--- clilang.rkt
+++ clilang.rkt
@@ -19,12 +19,26 @@
                      #%module-begin)
          #%top #%app #%datum #%top-interaction)
 
+;; cli struct specification
+;;   - maybe (make-cli) prior to calling (run ...)
+;;     to create a (cli ...) struct
+;;     which could be introspected and independently executed
+;;     in requiring modules
+;;     -> define a function (cli-spec) at the module level
+;;        in read-syntax, which should return the cli instance
+;;        and it should be (provide ...)ed at this level
+;;   - and then if that works out, maybe make all of the
+;;     syntactic forms here simply mutate the global cli instance
+;;     instead of setting individual variables
+;;     (could use lenses to make this cleanish)
+
 (define (read-syntax path port)
   (define src-datums (sequence->list (in-port read port)))
   (define module-datum `(module cli-mod "clilang.rkt"
                           (define ~program "")
                           (define ~usage-help "")
                           (define ~once-each (list))
+                          (define ~once-any (make-hash))
 
                           ,@src-datums))
   (datum->syntax #f module-datum))
@@ -48,27 +62,49 @@
      #'(begin
          (define param-name (make-parameter #f))
          (set! ~once-each
-               (cons (list name short-flag verbose-flag description handler #'handler)
-                     ~once-each))))])
+               (cons (list 'name short-flag verbose-flag description handler #'handler)
+                     ~once-each))))]
+  [(_ name ((~datum one-of) (short-flag verbose-flag description handler)
+                            ...))
+   (with-syntax ([~once-any (datum->syntax this-syntax '~once-any)]
+                 [param-name (datum->syntax this-syntax #'name)])
+     #'(begin
+         (define param-name (make-parameter #f))
+         (hash-set! ~once-any
+                    'name
+                    (list
+                     (list 'name short-flag verbose-flag description handler #'handler)
+                     ...))))])
 
 (define-simple-macro (cli-module-begin EXPR ...)
   (#%module-begin
    EXPR ...))
 
+(define (read-spec spec)
+  (list (list (second spec) (third spec))
+        (lambda (flg . args)
+          (apply (fifth spec) args))
+        (list (fourth spec) "NAME")))
+
+(define (read-specs specs header)
+  (cons header
+        (for/list ([spec specs])
+          (read-spec spec))))
+
 (define-syntax-parser run
   [(_ ([arg desc] ...) body ...)
    (with-syntax ([~program (datum->syntax this-syntax '~program)]
                  [~usage-help (datum->syntax this-syntax '~usage-help)]
-                 [~once-each (datum->syntax this-syntax '~once-each)])
+                 [~once-each (datum->syntax this-syntax '~once-each)]
+                 [~once-any (datum->syntax this-syntax '~once-any)])
      #'(module+ main
-         (let* ([once-eaches (for/list ([spec ~once-each])
-                               (list (list (second spec) (third spec))
-                                     (lambda (flg . args)
-                                       (apply (fifth spec) args))
-                                     (list (fourth spec) "NAME")))]
+         (let* ([once-eaches (read-specs ~once-each 'once-each)]
+                [once-anies
+                 (for/list ([specs (hash-values ~once-any)])
+                   (read-specs specs 'once-any))]
                 [table `((usage-help ,~usage-help)
-                        (once-each
-                         ,@once-eaches))])
+                         ,once-eaches
+                         ,@once-anies)])
            (parse-command-line ~program
                                (current-command-line-arguments)
                                table
--- dummy.rkt
+++ dummy.rkt
@@ -5,13 +5,40 @@
 
 (usage-help "This is a fun script.")
 
+(flag length
+      ("-l" "--length")
+      "Length to search up to"
+      (λ (~l)
+        (length ~l)))
+
 (flag depth
-      ("-d" "--depth")
-      "Depth to search up to"
-      (λ (~d)
-        (depth ~d)))
+      (one-of ("-d"
+               "--depth"
+               "Depth to search up to"
+               (λ (~d)
+                 (depth ~d)))
+              ("-g"
+               "--height"
+               "Height to search up to"
+               (λ (~h)
+                 (depth ~h)))))
+
+(flag width
+      (one-of ("-w"
+               "--width"
+               "Width to search up to"
+               (λ (~w)
+                 (width ~w)))
+              ("-b"
+               "--breadth"
+               "Breadth to search up to"
+               (λ (~b)
+                 (width ~b)))))
 
 (run ()
      (displayln "Hello there!")
      (displayln ~once-each)
-     (displayln (depth)))
+     (displayln ~once-any)
+     (displayln (length))
+     (displayln (depth))
+     (displayln (width)))
